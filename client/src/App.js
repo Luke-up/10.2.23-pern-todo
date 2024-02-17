844ec81bc5a568cefd1a11b55d2e70b230c14e16
Luke <painelukeb@gmail.com>
2024-02-17 22:03:40 +0200

    Add client side

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import './App.css';
+
+// Components 
+import InputTodo from "./components/inputTodo";
+
+function App() {
+  return (
+  <Fragment>
+    <div className="container">
+      <InputTodo/>  
+    </div>
+  </Fragment>
+  )
+}
+
+export default App;
